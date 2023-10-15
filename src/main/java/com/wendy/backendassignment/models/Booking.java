cda439ca81e1def479a1e184708d818d7b41bf1a
wen2811 <wenbehr@gmail.com>
2023-10-15 14:25:26 +0200

    creating the Booking Class

---
+++ src/main/java/com/wendy/backendassignment/models/Booking.java
@@ -0,0 +1,46 @@
+package com.wendy.backendassignment.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "bookings")
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate date;
+    private double totalAmount;
+
+
+    @Enumerated(EnumType.STRING)
+    private BookingStatus bookingStatus;
+
+    ///@OneToOne(mappedBy = "booking")
+    //private Invoice invoice;
+
+   // @OneToMany(mappedBy = "booking")
+   // private List<BookingTreatment> bookingTreatments;
+
+
+   // @ManyToOne
+    //private Customer customer;
+
+
+    //@ManyToOne
+   // private User user;
+
+    public Booking(LocalDate date, double totalAmount ){
+        this.date = date;
+        this.totalAmount = totalAmount;
+    }
+}
