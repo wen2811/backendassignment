da006b38c04207edb916786768e63fcc30270755
wen2811 <wenbehr@gmail.com>
2023-10-15 22:29:33 +0200

    feat- adding Service and Controller in Treatment -adding basic Crud operations

---
+++ src/main/java/com/wendy/backendassignment/controllers/TreatmentController.java
@@ -0,0 +1,70 @@
+package com.wendy.backendassignment.controllers;
+
+import com.wendy.backendassignment.dtos.TreatmentDto;
+import com.wendy.backendassignment.exception.RecordNotFoundException;
+import com.wendy.backendassignment.models.TreatmentType;
+import com.wendy.backendassignment.services.TreatmentService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/treatments")
+public class TreatmentController {
+    private final TreatmentService treatmentService;
+
+    public TreatmentController(TreatmentService treatmentService) {
+        this.treatmentService = treatmentService;
+    }
+
+    //Read
+    @GetMapping("/")
+    public ResponseEntity<List<TreatmentDto>> getAllTreatments() {
+        return ResponseEntity.ok().body(treatmentService.getAllTreatments());
+    }
+
+    @GetMapping("/{type}")
+    public ResponseEntity<TreatmentDto> getTreatmentsByType(@PathVariable TreatmentType type) throws RecordNotFoundException {
+        return ResponseEntity.ok().body(treatmentService.getTreatmentsByType(type));
+    }
+
+    //Create
+    @PostMapping("")
+    public ResponseEntity<Object> addTreatment(@Valid @RequestBody TreatmentDto treatmentDto, BindingResult br) {
+        if(br.hasFieldErrors()) {
+            StringBuilder sb = new StringBuilder();
+            for (FieldError fe : br.getFieldErrors()) {
+                sb.append(fe.getField() + ": ");
+                sb.append(fe.getDefaultMessage());
+                sb.append("\n");
+            }
+            return ResponseEntity.badRequest().body(sb.toString());
+        } else {
+            TreatmentDto addedTreatment = treatmentService.addTreatment(treatmentDto);
+            URI uri = URI.create(String.valueOf(ServletUriComponentsBuilder.fromCurrentRequest().path("/" + addedTreatment.id)));
+            return ResponseEntity.created(uri).body(addedTreatment);
+        }
+    }
+
+    //Update
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateTreatment(@PathVariable("id") Long id, @RequestBody TreatmentDto treatmentDto) {
+        treatmentService.updateTreatment(id, treatmentDto);
+        return ResponseEntity.ok(treatmentDto);
+    }
+
+    //Delete
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteTreatment(@PathVariable Long id) throws RecordNotFoundException  {
+        treatmentService.deleteTreatment(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
--- src/main/java/com/wendy/backendassignment/services/TreatmentService.java
+++ src/main/java/com/wendy/backendassignment/services/TreatmentService.java
@@ -1,9 +1,15 @@
 package com.wendy.backendassignment.services;
 
 import com.wendy.backendassignment.dtos.TreatmentDto;
+import com.wendy.backendassignment.exception.RecordNotFoundException;
 import com.wendy.backendassignment.models.Treatment;
+import com.wendy.backendassignment.models.TreatmentType;
 import com.wendy.backendassignment.repositories.TreatmentRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TreatmentService {
@@ -12,6 +18,72 @@ public class TreatmentService {
     public TreatmentService(TreatmentRepository treatmentRepository) {
         this.treatmentRepository = treatmentRepository;
     }
+
+    //Read
+    public List<TreatmentDto> getAllTreatments() {
+        Iterable<Treatment> treatments = treatmentRepository.findAll();
+        List<TreatmentDto> treatmentDtos = new ArrayList<>();
+
+        for (Treatment treatment: treatments) {
+            treatmentDtos.add(transferTreatmentToDto(treatment));
+        }
+
+        return treatmentDtos;
+    }
+
+    public TreatmentDto getTreatmentsByType(TreatmentType type) throws RecordNotFoundException {
+        Optional<Treatment> treatmentOptional = treatmentRepository.findTreatmentsByType(type);
+
+        if(treatmentOptional.isEmpty()) {
+            throw new RecordNotFoundException("There's no treatment to find with this type: " + type);
+        }
+        Treatment treatment = treatmentOptional.get();
+
+        return transferTreatmentToDto(treatment);
+    }
+
+    //Create
+    public TreatmentDto addTreatment(TreatmentDto treatmentDto) {
+        Treatment treatment = transferDtoToTreatment(treatmentDto);
+        treatmentRepository.save(treatment);
+
+        return transferTreatmentToDto(treatment);
+    }
+
+    //Update
+    public void updateTreatment(Long id, TreatmentDto treatmentDto) {
+        Treatment existingTreatment = treatmentRepository.findById(id)
+                .orElseThrow(() -> new RecordNotFoundException("There's no treatment found with this id " + id));
+        if (treatmentDto.getDescription() != null) {
+            existingTreatment.setDescription(treatmentDto.getDescription());
+        }
+        if (treatmentDto.getDuration() != 0.0) {
+            existingTreatment.setDuration(treatmentDto.getDuration());
+        }
+        if (treatmentDto.getName() != null) {
+            existingTreatment.setName(treatmentDto.getName());
+        }
+        if (treatmentDto.getType() != null) {
+            existingTreatment.setType(treatmentDto.getType());
+        }
+        if (treatmentDto.getPrice() != 0.0) {
+            existingTreatment.setPrice(treatmentDto.getPrice());
+        }
+        treatmentRepository.save(existingTreatment);
+    }
+
+    //Delete
+    public void deleteTreatment(Long id) throws RecordNotFoundException {
+        Optional<Treatment> optionalTreatment = treatmentRepository.findById(id);
+        if(optionalTreatment.isEmpty()) {
+            throw new RecordNotFoundException("There is no booking found with id: " + id);
+        }
+        Treatment treatment = optionalTreatment.get();
+        treatmentRepository.delete(treatment);
+    }
+
+
+
 
     public TreatmentDto transferTreatmentToDto (Treatment treatment){
         TreatmentDto treatmentDto = new TreatmentDto();
